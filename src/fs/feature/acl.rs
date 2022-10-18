6da620f9947beb00d17b445c24a778c0a4bb6cc4
Steve Lau <stevelauc@outlook.com>
2022-10-18 18:40:23 +0800

    feat: extended ACL

--- src/fs/feature/acl.rs
+++ src/fs/feature/acl.rs
@@ -1,13 +1,24 @@
 //! Extended ACL support for Linux
 
-use posix_acl::{PosixACL, Qualifier::Mask};
 use std::{fs::metadata, path::Path};
+use posix_acl::{
+    Qualifier::{GroupObj, Other, UserObj, Mask},
+    PosixACL,
+};
+
+pub fn dir_has_default_acl<P: AsRef<Path>>(path: P) -> bool {
+    let default_acl_reader = PosixACL::read_default_acl(path.as_ref()).unwrap();
+
+    default_acl_reader.get(UserObj).is_some() ||
+        default_acl_reader.get(GroupObj).is_some() ||
+        default_acl_reader.get(Other).is_some()
+}
+
+pub fn file_has_extended_access_acl<P: AsRef<Path>>(path: P) -> bool {
+    PosixACL::read_acl(path.as_ref()).unwrap().get(Mask).is_some()
+}
 
 pub fn has_acl<P: AsRef<Path>>(path: P) -> bool {
-    metadata(path.as_ref()).unwrap().is_dir()
-        && PosixACL::read_default_acl(path.as_ref())
-            .unwrap()
-            .get(Mask)
-            .is_some()
-        || (PosixACL::read_acl(path.as_ref()).unwrap().get(Mask)).is_some()
+    metadata(path.as_ref()).unwrap().is_dir() && dir_has_default_acl(path.as_ref()) ||
+        file_has_extended_access_acl(path.as_ref())
 }
